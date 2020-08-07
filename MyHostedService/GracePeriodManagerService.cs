7da7495825f333585457248459be138048b64250
Marcos Soares <slipmp@gmail.com>
2020-08-07 16:55:30 -0700

    Adding all possible examples provided by Microsoft on IHostedService implementations

---
+++ MyHostedService/GracePeriodManagerService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyHostedService
+{
+    public class GracePeriodManagerService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine($"GracePeriodManagerService is starting.");
+
+            stoppingToken.Register(() =>
+                Console.WriteLine($" GracePeriod background task is stopping."));
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"GracePeriod task doing background work.");
+
+                // This eShopOnContainers method is querying a database table
+                // and publishing events into the Event Bus (RabbitMQ / ServiceBus)
+                CheckConfirmedGracePeriodOrders();
+
+                await Task.Delay(3000, stoppingToken);
+            }
+
+            Console.WriteLine($"GracePeriod background task is stopping.");
+        }
+
+        private void CheckConfirmedGracePeriodOrders()
+        {
+            Console.WriteLine("CheckConfirmedGracePeriodOrders completed.");
+        }
+    }
+}
--- MyHostedService/Program.cs
+++ MyHostedService/Program.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MyHostedService
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            using var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    #region snippet3
+                    services.AddSingleton<MonitorLoop>();
+                    services.AddHostedService<QueuedHostedService>();
+                    services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+                    #endregion
+
+                    #region snippet1
+                    services.AddHostedService<TimedHostedService>();
+                    #endregion
+
+                    #region snippet2
+                    services.AddHostedService<ConsumeScopedServiceHostedService>();
+                    services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
+                    #endregion
+                })
+                .Build();
+
+            await host.StartAsync();
+
+            #region snippet4
+            var monitorLoop = host.Services.GetRequiredService<MonitorLoop>();
+            monitorLoop.StartMonitorLoop();
+            #endregion
+
+            await host.WaitForShutdownAsync();
         }
     }
 }
