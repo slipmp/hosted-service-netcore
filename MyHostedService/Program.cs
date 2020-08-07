0b36920b6e3b0961c2bb565bb49bd7a3f2ce798c
Marcos Soares <slipmp@gmail.com>
2020-08-07 15:49:55 -0700

    Adding hello world Console Application as first step

---
+++ MyHostedService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyHostedService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
